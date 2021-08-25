b668492f001b88f7b940f7cbcb853cea7da36a08
PM-cutedogpics <pete_choi@dlsu.edu.ph>
2021-08-26 03:06:41 +0800

    Revert "DatabaseHelper completed with collections and commands (add, update)"
    This reverts commit 2161de8a7d9f5eadcb23abc4cb2b6ea8e7faedf3.

--- app/src/main/java/com/mobdeve/s12/tilttosurvive/StoreActivity.java
+++ app/src/main/java/com/mobdeve/s12/tilttosurvive/StoreActivity.java
@@ -4,17 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageButton;
-import android.widget.Toast;
 
-public class StoreActivity extends AppCompatActivity {
-
-    private ImageButton ibtnFreeze;
-    private ImageButton ibtnNuke;
-    private ImageButton ibtnLazer;
-    private ImageButton ibtnForceField;
-    private ImageButton ibtnHaste;
-    private ImageButton ibtnSpeedDown;
+public class storeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,16 +19,5 @@ public class StoreActivity extends AppCompatActivity {
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-
-
-    }
-
-    private void finishUpdateStore(long result) {
-        if (result == -1) {
-            Toast.makeText(this, "Failed to Buy Power Up", Toast.LENGTH_SHORT).show();
-        }
-        else {
-            Toast.makeText(this, "Power Up Succesfully Bought", Toast.LENGTH_SHORT).show();
-        }
     }
 }
